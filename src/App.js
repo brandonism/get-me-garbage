8c740170c39f1c99be90443dc03cc77568e1ffff
brandonism <brandonbpost@gmail.com>
2021-02-27 17:54:18 -0500

    added distanceMatrix from maps api and wrapped service calls in promises

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Button from 'react-bootstrap/Button';
-import { Hots } from './Data.js'
 import Map from './Map.js'
 
 class App extends React.Component {
@@ -10,33 +9,15 @@ class App extends React.Component {
         this.state = {show:false,origin:null};
         navigator.geolocation.getCurrentPosition(
             (position) => {
-                const origin = {lat:position.coords.latitude,lng:position.coords.longitude};
-                this.setState({ 
-                    origin:origin,
-                    destination:this.getClosest(origin,Hots.data)
-                });
-                console.log("position found");
+                this.setState({origin:{name:'You',lat:position.coords.latitude,lng:position.coords.longitude}});
             },(error) => { 
                 console.log(error); 
             },{timeout:3000});
     }
 
-    getClosest(origin,destinations) {
-        var min = Number.MAX_VALUE;
-        var destination;
-        destinations.forEach(dest => {
-            var distance = Math.sqrt(Math.pow(origin.lat - dest.lat, 2) + Math.pow(origin.lng - dest.lng, 2));
-            if (distance < min) {
-                destination = dest;
-                min = distance;
-            }
-        });
-        return destination;
-    }
-
     checkOrigin() {
         if (this.state.origin === null) {
-            alert("Turn Location Services on");
+            alert('Turn Location Services on');
         } else {
             this.setState({show:!this.state.show})
         }
@@ -45,8 +26,12 @@ class App extends React.Component {
     render() {
         return (
             <>
-            {!this.state.show && <Button className="finder" variant="dark" onClick={() => this.checkOrigin()}><h1>Find the Nearest Garbage Plate</h1></Button>}
-            {this.state.show && <Map origin={this.state.origin} destination={this.state.destination} />}
+            {!this.state.show && 
+                <Button className='finder' variant='dark' onClick={() => this.checkOrigin()}>
+                    <h1>Find the Nearest Garbage Plate</h1>
+                </Button>}
+            {this.state.show && 
+                <Map origin={this.state.origin} />}
             </>
         );
     }
--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { MapStyles } from './Data.js'
+import { Hots } from './Data.js'
 /* global google */
 
-const lineColor = "#409F9F";
-const labelColor = "#ffAA55";
+const lineColor = '#409F9F';
+const labelColor = '#ffAA55';
 
 export class Map extends React.Component {
     constructor(props) {
@@ -12,68 +13,94 @@ export class Map extends React.Component {
     }
 
     componentDidMount() {
-        var map = new google.maps.Map(
+        let hots = this.striaghtDistances(this.props.origin,Hots.data)
+                    .sort((a,b) => a.striaghtDistance - b.striaghtDistance)
+                    .slice(0,9);//distanceMatrix is limited to 25, slicing to 10 for speed
+
+        this.drivingDistances(this.props.origin,hots).then(results=>{
+            results.forEach((distance,index) => hots[index].drivingDistance = distance);
+            const closest = hots.sort((a,b) => a.drivingDistance - b.drivingDistance)[0];
+            this.directions(this.props.origin,closest).then(result => this.createMap(closest,result))
+        });
+    }
+
+    striaghtDistances(origin,destinations) {
+        return destinations.map(dest => {
+            dest.striaghtDistance = Math.sqrt(Math.pow(origin.lat - dest.lat, 2) 
+                                            + Math.pow(origin.lng - dest.lng, 2));
+            return dest
+        });
+    }
+
+    drivingDistances(origin, destinations) {
+        const service = new google.maps.DistanceMatrixService();
+        return new Promise((resolve, reject) => {
+            service.getDistanceMatrix({
+                origins: [origin],
+                destinations: destinations,
+                travelMode: google.maps.TravelMode.DRIVING,
+            }, (response, status) => {
+                if(status === 'OK') {
+                    resolve(response.rows[0].elements.map(resp=>resp.distance.value));
+                } else {
+                    reject(new Error('Error getting distance'));
+                }
+            });
+        });
+    }
+
+    directions(origin,destination) {
+        const service = new google.maps.DirectionsService();
+        return new Promise((resolve, reject) => {
+            service.route({
+                origin: origin,
+                destination: destination,
+                travelMode: google.maps.TravelMode.DRIVING
+            },(response, status) => {
+                if (status === google.maps.DirectionsStatus.OK) {
+                    resolve(response);
+                } else {
+                    reject(new Error('Error getting directions'));
+                }
+            });
+        });
+    }
+
+    createMap(destination,directions) {
+        let map = new google.maps.Map(
             this.mapref.current, {
             zoom: 10,
             center: this.props.origin,
-            mapTypeId: "terrain",
+            mapTypeId: 'terrain',
             styles: MapStyles.data
         });
-        new google.maps.Marker({
-            position: this.props.origin,
-            map,
-            title: "You",
-            icon: {
-                path: google.maps.SymbolPath.CIRCLE,
-                strokeColor: lineColor,
-                scale: 10,
-            },
-            label: {
-                text: "You",
-                color: labelColor,
-                fontSize: "16px",
-                fontWeight: "bold"
-              },
-        });
-        new google.maps.Marker({
-            position: this.props.destination,
-            map,
-            title: this.props.destination.name,
-            icon: {
-                path: google.maps.SymbolPath.CIRCLE,
-                strokeColor: lineColor,
-                scale: 10,
-            },
-            label: {
-                text: this.props.destination.name,
-                color: labelColor,
-                fontSize: "16px",
-                fontWeight: "bold"
-              },
-        });
-        var directionsMap = new google.maps.DirectionsRenderer({
+        this.addMarker(this.props.origin,map);
+        this.addMarker(destination,map);
+        let directionsMap = new google.maps.DirectionsRenderer({
             suppressMarkers: true,
             polylineOptions: { strokeColor: lineColor } 
         });
         directionsMap.setMap(map);
-        this.getDirections(this.props.origin,this.props.destination,directionsMap);
+        directionsMap.setDirections(directions);
     }
 
-    getDirections(origin,destination,map) {
-        const directionsService = new google.maps.DirectionsService();
-
-        directionsService.route({
-                origin: origin,
-                destination: destination,
-                travelMode: google.maps.TravelMode.DRIVING
-            },(result, status) => {
-                if (status === google.maps.DirectionsStatus.OK) {
-                    map.setDirections(result);
-                } else {
-                    console.error(result);
-                }
-            }
-        );
+    addMarker(location,map) {
+        new google.maps.Marker({
+            position: location,
+            map,
+            title: location.name,
+            icon: {
+                path: google.maps.SymbolPath.CIRCLE,
+                strokeColor: lineColor,
+                scale: 10,
+            },
+            label: {
+                text: location.name,
+                color: labelColor,
+                fontSize: '16px',
+                fontWeight: 'bold'
+            },
+        });
     }
 
     render() {
